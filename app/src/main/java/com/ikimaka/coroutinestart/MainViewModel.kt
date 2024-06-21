fd6aec98662a6ec24f9ffdf276a342ea77c70a61
ikimaka <ikimaka@yandex.ru>
2024-06-21 15:08:09 +0300

    Structured Concurrency

---
+++ app/src/main/java/com/ikimaka/coroutinestart/MainViewModel.kt
@@ -0,0 +1,48 @@
+package com.ikimaka.coroutinestart
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainViewModel: ViewModel() {
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+
+    fun method() {
+        val childJob1 = coroutineScope.launch {
+            delay(3000)
+            Log.d(LOG_TAG, "First coroutine finished")
+        }
+        val childJob2 = coroutineScope.launch {
+            delay(2000)
+            childJob1.cancel()
+            Log.d(LOG_TAG, "Second coroutine finished")
+            Log.d(LOG_TAG, "Parent job canceled: ${parentJob.isCancelled}")
+        }
+
+
+//        thread {
+//            Thread.sleep(1000)
+//            parentJob.cancel()
+//            Log.d(LOG_TAG, "Parent job is active ${parentJob.isActive}")
+//        }
+        Log.d(LOG_TAG, parentJob.children.contains(childJob1).toString())
+        Log.d(LOG_TAG, parentJob.children.contains(childJob2).toString())
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+    companion object {
+        private const val LOG_TAG = "MainViewModelTest"
+    }
+}
